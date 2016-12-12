5177cd4f74f3b1c672944e702c00c6e9f1aa74e9
higucheese <t.higuchi.eeic@gmail.com>
2016-12-12 16:22:15 +0900

    256*256で切り取るように変更した

--- on_machine/capture.py
+++ on_machine/capture.py
@@ -5,26 +5,15 @@ camera = cv2.VideoCapture(0)
 if camera.isOpened() is False:
     raise("camera IO Error")
 
-# cv2.namedWindow("camera", cv2.WINDOW_AUTOSIZE)
-
 frame = camera.read()[1]
-cv2.imwrite("frame.png", frame)
+height, width, channels = frame.shape
+CUT_SIZE = 256
+t_height = (height - CUT_SIZE) / 2
+t_width = (width - CUT_SIZE) / 2
+clp = img[t_height:t_height + CUT_SIZE, t_width:t_width + CUT_SIZE]
+
+cv2.imwrite("clip.png", clp)
+
 camera.release()
-'''
-count = 0
-while True:
-    ret, image = camera.read()
-    if ret is False:
-        continue
 
-    # cv2.imshow("camera", image)
-
-    k = cv2.waitKey(10)
-    if k is 115:
-        cv2.imwrite("data.png", image)
-        count += 1
-        print count
-    elif k == 27:
-        break
-'''
 cv2.destroyAllWindows()
