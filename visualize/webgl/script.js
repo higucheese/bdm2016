ac8d7ae22ce7bb40b6a811ac041b4a74a8822ed2
sakaguchi <e.st1.ta2altr5@gmail.com>
2016-12-15 16:39:01 +0900

    add ambient lighting

--- visualize/webgl/script.js
+++ visualize/webgl/script.js
@@ -14,31 +14,46 @@ onload = function(){
     // プログラムオブジェクトの生成とリンク
     var prg = create_program(v_shader, f_shader);
 
-    // attributeLocationの取得
-    var attLocation = new Array(2);
+    // attributeLocationを配列に取得
+    var attLocation = new Array();
     attLocation[0] = gl.getAttribLocation(prg, 'position');
-    attLocation[1] = gl.getAttribLocation(prg, 'color');
+    attLocation[1] = gl.getAttribLocation(prg, 'normal');
+    attLocation[2] = gl.getAttribLocation(prg, 'color');
 
-    // attributeの要素数(この場合は xyz の3要素)
-    var attStride = new Array(2);
+    // attributeの要素数を配列に格納
+    var attStride = new Array();
     attStride[0] = 3;
-    attStride[1] = 4;
+    attStride[1] = 3;
+    attStride[2] = 4;
 
     // トーラスの頂点データを生成
-	var torusData = torus(32, 32, 1.0, 2.0);
-	var position = torusData[0];
-	var color = torusData[1];
-	var index = torusData[2];
+    var torusData = torus(32, 32, 1.0, 2.0);
+    var position = torusData[0];
+    var normal = torusData[1];
+    var color = torusData[2];
+    var index = torusData[3];
 
     // VBOの生成
     var pos_vbo = create_vbo(position);
+    var nor_vbo = create_vbo(normal);
     var col_vbo = create_vbo(color);
-    set_attribute([pos_vbo, col_vbo], attLocation, attStride);
-    // IBOをバウンド
+
+    // VBO を登録する
+    set_attribute([pos_vbo, nor_vbo, col_vbo], attLocation, attStride);
+
+    // IBOの生成
     var ibo = create_ibo(index);
 
+    // IBOをバインドして登録する
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-    var uniLocation = gl.getUniformLocation(prg, 'mvpMatrix');
+
+    var ambientColor = [0.1, 0.1, 0.1, 1.0];
+    // uniformLocationを配列に取得
+    var uniLocation = new Array();
+    uniLocation[0] = gl.getUniformLocation(prg, 'mvpMatrix');
+    uniLocation[1] = gl.getUniformLocation(prg, 'invMatrix');
+    uniLocation[2] = gl.getUniformLocation(prg, 'lightDirection');
+    uniLocation[3] = gl.getUniformLocation(prg, 'ambientColor');
 
     // minMatrix.js を用いた行列関連処理
     // matIVオブジェクトを生成
@@ -50,34 +65,59 @@ onload = function(){
     var pMatrix = m.identity(m.create());
     var vpMatrix = m.identity(m.create());
     var mvpMatrix = m.identity(m.create());
+    var invMatrix = m.identity(m.create());
 
-    // あらかじめView x Projection Matrixを用意
+    // ビュー×プロジェクション座標変換行列
     m.lookAt([0.0, 0.0, 20.0], [0, 0, 0], [0, 1, 0], vMatrix);
     m.perspective(45, c.width / c.height, 0.1, 100, pMatrix);
     m.multiply(pMatrix, vMatrix, vpMatrix);
 
+    // 平行光源の向き
+    var lightDirection = [-0.5, 0.5, 0.5];
+
+    // カウンタの宣言
     var count = 0;
 
-    (function() {
+    // カリングと深度テストを有効にする
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.enable(gl.CULL_FACE);
+
+    // 恒常ループ
+    (function(){
         // canvasを初期化
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
         gl.clearDepth(1.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        count ++;
+        // カウンタをインクリメントする
+        count++;
 
+        // カウンタを元にラジアンを算出
         var rad = (count % 360) * Math.PI / 180;
 
+        // モデル座標変換行列の生成
         m.identity(mMatrix);
         m.rotate(mMatrix, rad, [0, 1, 1], mMatrix);
         m.multiply(vpMatrix, mMatrix, mvpMatrix);
-        gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
 
+        // モデル座標変換行列から逆行列を生成
+        m.inverse(mMatrix, invMatrix);
+
+        // uniform変数の登録
+        gl.uniformMatrix4fv(uniLocation[0], false, mvpMatrix);
+        gl.uniformMatrix4fv(uniLocation[1], false, invMatrix);
+        gl.uniform3fv(uniLocation[2], lightDirection);
+        gl.uniform4fv(uniLocation[3], ambientColor);
+
+        // モデルの描画
         gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
 
+        // コンテキストの再描画
         gl.flush();
 
-        setTimeout(arguments.callee, 100/30);
+        // ループのために再帰呼び出し
+        setTimeout(arguments.callee, 100 / 30);
     })();
 
     // シェーダを生成する関数
@@ -187,25 +227,27 @@ onload = function(){
     }
 
     // IBOを生成する関数
-    function create_ibo(data){
-        // バッファオブジェクトの生成
-        var ibo = gl.createBuffer();
+   function create_ibo(data){
+       // バッファオブジェクトの生成
+       var ibo = gl.createBuffer();
 
-        // バッファをバインドする
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+       // バッファをバインドする
+       gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
 
-        // バッファにデータをセット
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
+       // バッファにデータをセット
+       gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
 
-        // バッファのバインドを無効化
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+       // バッファのバインドを無効化
+       gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
-        // 生成したIBOを返して終了
-        return ibo;
-    }
+       // 生成したIBOを返して終了
+       return ibo;
+   }
 
+    // トーラスを生成する関数
     function torus(row, column, irad, orad){
-        var pos = new Array(), col = new Array(), idx = new Array();
+        var pos = new Array(), nor = new Array(),
+        col = new Array(), idx = new Array();
         for(var i = 0; i <= row; i++){
             var r = Math.PI * 2 / row * i;
             var rr = Math.cos(r);
@@ -215,7 +257,10 @@ onload = function(){
                 var tx = (rr * irad + orad) * Math.cos(tr);
                 var ty = ry * irad;
                 var tz = (rr * irad + orad) * Math.sin(tr);
+                var rx = rr * Math.cos(tr);
+                var rz = rr * Math.sin(tr);
                 pos.push(tx, ty, tz);
+                nor.push(rx, ry, rz);
                 var tc = hsva(360 / column * ii, 1, 1, 1);
                 col.push(tc[0], tc[1], tc[2], tc[3]);
             }
@@ -227,8 +272,9 @@ onload = function(){
                 idx.push(r + column + 1, r + column + 2, r + 1);
             }
         }
-        return [pos, col, idx];
+        return [pos, nor, col, idx];
     }
+
     function hsva(h, s, v, a){
         if(s > 1 || v > 1 || a > 1){return;}
         var th = h % 360;
