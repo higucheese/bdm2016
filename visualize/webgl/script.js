5fb7770eb023d7f9d016c10b4c0d1b1dc3d9df96
sakaguchi <e.st1.ta2altr5@gmail.com>
2016-12-15 13:06:06 +0900

    move triangles

--- visualize/webgl/script.js
+++ visualize/webgl/script.js
@@ -76,28 +76,51 @@ onload = function(){
     // あらかじめView x Projection Matrixを用意
     m.lookAt([0.0, 0.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
     m.perspective(90, c.width / c.height, 0.1, 100, pMatrix);
-    m.multiply(pMatrix, vMatrix, vpMatrix); 
-
-    // 一つ目のモデルの位置
-    m.translate(mMatrix, [1.5, 0.0, 0.0], mMatrix);
-    m.multiply(vpMatrix, mMatrix, mvpMatrix);
-    // uniformLocationの取得
+    m.multiply(pMatrix, vMatrix, vpMatrix);
     var uniLocation = gl.getUniformLocation(prg, 'mvpMatrix');
-    // uniformLocationへ座標変換行列を登録
-    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);    
-    // モデルの描画
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
 
-    // 2つ目のモデルの位置
-    m.identity(mMatrix);
-    m.translate(mMatrix, [-1.5, 0.0, 0.0], mMatrix);
-    m.multiply(vpMatrix, mMatrix, mvpMatrix);
-    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    var count = 0;
 
-    
-    // コンテキストの再描画
-    gl.flush();
+    (function() {
+        // canvasを初期化
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearDepth(1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        count ++;
+
+        var rad = (count % 360) * Math.PI / 180;
+
+        var x = Math.cos(rad);
+        var y = Math.sin(rad);
+        m.identity(mMatrix);
+        m.translate(mMatrix, [x, y+1.0, 0.0], mMatrix);
+
+        m.multiply(vpMatrix, mMatrix, mvpMatrix);
+        gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+        m.identity(mMatrix);
+        m.translate(mMatrix, [1.0, -1.0, 0.0], mMatrix);
+        m.rotate(mMatrix, rad, [0, 1, 0], mMatrix);
+
+        m.multiply(vpMatrix, mMatrix, mvpMatrix);
+        gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+        var s = Math.sin(rad) + 1.0;
+        m.identity(mMatrix);
+        m.translate(mMatrix, [-1.0, -1.0, 0.0], mMatrix);
+        m.scale(mMatrix, [s, s, 0.0], mMatrix);
+
+        m.multiply(vpMatrix, mMatrix, mvpMatrix);
+        gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+        gl.flush();
+
+        setTimeout(arguments.callee, 100/30);
+    })();
     
     // シェーダを生成する関数
     function create_shader(id){
