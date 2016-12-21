a28f0cbcee7ecd3778ddf52dcc62197d618bb1bb
higucheese <t.higuchi.eeic@gmail.com>
2016-12-22 00:34:47 +0900

    少しdeg2xyz.pyの出力を変更

--- recognition/tips/deg2xyz.py
+++ recognition/tips/deg2xyz.py
@@ -16,8 +16,8 @@ for line in fr:
     x = r * math.sin(theta) * math.cos(phi)
     y = r * math.sin(theta) * math.sin(phi)
     z = r * math.cos(theta)
-    # newline = str(x)+"\t"+str(y)+"\t"+str(z)+"\n"
-    newline = str(spl[0])+","+str(spl[1])+","+str(int(spl[2]) - 1)+"\n"
+    newline = str(x)+"\t"+str(y)+"\t"+str(z)+"\n"
+    # newline = str(spl[0])+","+str(spl[1])+","+str(int(spl[2]) - 1)+"\n"
     # newline = str(spl[0])+"\t"+str(spl[2])
     fw.write(newline)
 
