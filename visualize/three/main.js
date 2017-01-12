232ddb59f511d1ec195ce986c3b177b610733240
sakaguchi <e.st1.ta2altr5@gmail.com>
2017-01-13 03:19:20 +0900

    DeviceOrientationControllerを使って画面回転ができた

--- visualize/three/main.js
+++ visualize/three/main.js
@@ -19,6 +19,7 @@ var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHei
 
 // マウスでのカメラのコントロール
 // var controls = new THREE.TrackballControls(camera);
+var gcontrols = new THREE.DeviceOrientationControls(camera);
 
 /*
 * テクスチャの作成
@@ -71,21 +72,27 @@ loader.load('back.png', texture => { // onLoad
 
 /*
 * カメラ周り
+* もう使ってないけど
 */
 
 var threshold = 0.3;
 var gravity = new THREE.Vector3(0, 0, 0);
+var rotation = new THREE.Vector3(0, 0, 0);
 var g_v = new THREE.Vector3(0, 0, 0);
+var rot_z_int = 0;
+var old_time = 0;
 
 camera.position.copy(new THREE.Vector3(0, 0, 0));
 camera.lookAt(new THREE.Vector3(50, 50, 0));
 
 window.addEventListener("devicemotion", function(evt){
     var acc_g = evt.accelerationIncludingGravity;
-    var rota_r = evt.rotationRate;
+    var rot_r = evt.rotationRate;
+    dt = clock.getElapsedTime() - old_time;
 
     if (Math.abs(gravity.x - acc_g.x) > threshold) {
-        g_v.x = acc_g.x / 9.8 * 150;
+        // g_v.x = acc_g.x / 9.8 * 150;
+        g_v.x = 100//rot_r.beta * dt;
         gravity.x = acc_g.x;
     }
     if (Math.abs(gravity.y - acc_g.y) > threshold) {
@@ -96,17 +103,13 @@ window.addEventListener("devicemotion", function(evt){
         g_v.y = acc_g.z / 9.8 * 150; // y upなのでyとzを逆にする
         gravity.z = acc_g.z;
     }
-
-    camera.position.copy(new THREE.Vector3(0, 0, 0));
-    camera.lookAt(g_v);
 }, true);
 
 animate(); // アニメーションを描画
-i = 0;
-var old_time = 0;
 function animate() {
     // アニメーション
-    requestAnimationFrame( animate );
-    // controls.update();
-    renderer.render( scene, camera );
+    requestAnimationFrame(animate);
+    // 加速度センサに応じてちょうどよく回転する
+    gcontrols.update();
+    renderer.render(scene, camera);
 }
