59fc750e8b8beba9803951acfe6bd66082a9ddcc
sakaguchi <e.st1.ta2altr5@gmail.com>
2017-01-13 08:48:15 +0900

    キューブの回転を追加

--- visualize/three/around_view.js
+++ visualize/three/around_view.js
@@ -15,9 +15,9 @@ var clock = new THREE.Clock();
 var scene = new THREE.Scene();
 
 // カメラを作成
-var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
+var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 2000);
 camera.position.copy(new THREE.Vector3(0, 0, 1));
-camera.lookAt(new THREE.Vector3(100, 0, 0));
+camera.lookAt(new THREE.Vector3(0, -100, 0));
 
 // マウスでのカメラのコントロール
 // var controls = new THREE.TrackballControls(camera);
@@ -30,45 +30,52 @@ var controls = new THREE.DeviceOrientationControls(camera);
 // loaderの作成
 var loader = new THREE.TextureLoader();
 loader.crossOrigin = '*';
-offset = {x: 200, y: 0, z: 0};
+offset = {x: 0, y: 0, z: -300};
+radian = 20 * Math.PI / 180;
 
 // 各画像を貼り付け
 loader.load('top.png', texture => { // onLoad
     var plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({map: texture}));
     plane.position.set(offset.x, 50 + offset.y, offset.z);
     plane.rotation.x = 90 * Math.PI / 180;
-    plane.rotation.z = 180 * Math.PI / 180;
+    plane.rotation.z = 180 * Math.PI / 180 - radian;
     scene.add(plane);
 });
 loader.load('bottom.png', texture => { // onLoad
     var plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({map: texture}));
     plane.rotation.x = 270 * Math.PI / 180;
-    plane.rotation.z = 270 * Math.PI / 180;
+    plane.rotation.z = 0 * Math.PI / 180 + radian;
     plane.position.set(offset.x, -50 + offset.y, offset.z);
     scene.add(plane);
 });
 loader.load('right.png', texture => { // onLoad
     var plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({map: texture}));
-    plane.rotation.y = 180 * Math.PI / 180;
-    plane.position.set(offset.x, offset.y, 50 + offset.z);
+    plane.rotation.y = 270 * Math.PI / 180 + radian;
+    plane.position.set(50 * Math.cos(radian) + offset.x, offset.y, -50 * Math.sin(radian) + offset.z);
     scene.add(plane);
 });
 loader.load('left.png', texture => { // onLoad
     var plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({map: texture}));
-    plane.rotation.y = 0 * Math.PI / 180;
-    plane.position.set(offset.x, offset.y, -50 + offset.z);
+    plane.rotation.y = 90 * Math.PI / 180 + radian;
+    plane.position.set(-50  * Math.cos(radian) + offset.x, offset.y, 50 * Math.sin(radian) + offset.z);
     scene.add(plane);
 });
 loader.load('front.png', texture => { // onLoad
     var plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({map: texture}));
-    plane.rotation.y = -90 * Math.PI / 180;
-    plane.position.set(50 + offset.x, offset.y, offset.z);
+    plane.rotation.y = 0 * Math.PI / 180 + radian;
+    plane.position.set(-50 * Math.sin(radian) + offset.x, offset.y, -50 * Math.cos(radian) + offset.z);
     scene.add(plane);
 });
 loader.load('back.png', texture => { // onLoad
     var plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({map: texture}));
-    plane.rotation.y = 90 * Math.PI / 180;
-    plane.position.set(-50 + offset.x, offset.y, offset.z);
+    plane.rotation.y = 180 * Math.PI / 180 + radian;
+    plane.position.set(50 * Math.sin(radian) + offset.x, offset.y, -50 * Math.cos(radian) + offset.z);
+    scene.add(plane);
+});
+loader.load('back.jpg', texture => { // onLoad
+    var plane = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000), new THREE.MeshBasicMaterial({map: texture}));
+    // plane.rotation.y = 90 * Math.PI / 180;
+    plane.position.set(0, 0, -500);
     scene.add(plane);
 });
 
