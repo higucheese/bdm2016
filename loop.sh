fa899e7bd9a5c542c24e2fa5528d59e1e97fd805
higucheese <t.higuchi.eeic@gmail.com>
2017-05-21 09:16:17 +0900

    add sh files

--- loop.sh
+++ loop.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 while :
     do
+        sudo route del default gw 192.168.2.1 &> /dev/null
         date >> progress.log
         /home/pi/bdm2016/capture.sh &> /dev/null
         /home/pi/bdm2016/upload.sh &> /dev/null
+        sleep 60
     done
--- upload.sh
+++ upload.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 sftp -b /home/pi/bdm2016/upload.bat ec2-aws
+#if [ $? != 0 ]; then
+#    sudo route del default gw 192.168.2.1
+#fi
